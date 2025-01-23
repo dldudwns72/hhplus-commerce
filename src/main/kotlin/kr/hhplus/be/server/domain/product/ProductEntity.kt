c424d6a942bb671f2e26db4c8be68c044a9adfda
dldudwns72 <dldudwns72@gmail.com>
2025-01-24 03:41:57 +0900

    :recycle: [refactor] 코드 정리

--- src/main/kotlin/kr/hhplus/be/server/domain/product/ProductEntity.kt
+++ src/main/kotlin/kr/hhplus/be/server/domain/product/ProductEntity.kt
@@ -2,7 +2,6 @@ package kr.hhplus.be.server.domain.product
 
 import jakarta.persistence.*
 import kr.hhplus.be.server.domain.common.BaseEntity
-import kr.hhplus.be.server.domain.order.OrderProductEntity
 
 @Entity
 @Table(name = "product")
@@ -12,13 +11,7 @@ class ProductEntity(
     var id: Long = 0L,
     val name: String,
     val price: Long,
-//    @OneToMany(mappedBy = "product", cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
-//    var orderProducts: MutableList<OrderProductEntity> = mutableListOf(),
-    @OneToOne(mappedBy = "product", fetch = FetchType.EAGER, cascade = [CascadeType.ALL])
+    @OneToOne(mappedBy = "product", fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
     var productInventory: ProductInventoryEntity
 ) : BaseEntity() {
-
-//    init {
-//        productInventory.product = this // 양방향 관계 설정
-//    }
 }
