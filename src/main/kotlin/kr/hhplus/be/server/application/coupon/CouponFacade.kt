74b934a230072ab9c4519a1ebae814a897e11f11
dldudwns72 <dldudwns72@gmail.com>
2025-02-07 08:55:13 +0900

    :sparkles: feat: 선착순 쿠폰 레디스 적용 및 테스트

--- src/main/kotlin/kr/hhplus/be/server/application/coupon/CouponFacade.kt
+++ src/main/kotlin/kr/hhplus/be/server/application/coupon/CouponFacade.kt
@@ -3,23 +3,29 @@ package kr.hhplus.be.server.application.coupon
 import jakarta.transaction.Transactional
 import kr.hhplus.be.server.domain.coupon.CouponUserService
 import kr.hhplus.be.server.domain.user.UserService
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
-/**
- * 유즈케이스 -> 문제를 해결하기 위한 요구사항
- * 파사드 -> 여러 객체간의 협력을 위한 디자인패턴 // 파사드는 인터페이스로 나타낸다.?
- */
 @Service
 class CouponFacade(
     private val userService: UserService,
     private val couponService: CouponService,
-    private val couponUserService: CouponUserService
+    private val couponUserService: CouponUserService,
+    private val couponRedisService: CouponRedisService
 ) {
+
     @Transactional
     fun issue(couponId: Long, userId: Long): CouponUserResult {
+        val timestamp = System.currentTimeMillis().toDouble()
         val user = userService.getUserById(userId)
-        val coupon  = couponService.getCoupon(couponId)
-        coupon.issue() // dirty-checking -> couponService.issue() 호출하고 transaction 단위를 service로 옮기는게 좋은가?
+        val coupon = couponService.getCoupon(couponId)
+
+        couponRedisService.issuedCouponUserVerify(coupon.id.toString(), user.id.toString())
+        couponRedisService.soldOutCouponUserVerify(coupon.id.toString(), coupon.maxCapacity)
+
+        couponRedisService.issue(coupon.id.toString(), userId.toString(), timestamp)
+        coupon.issue() // dirty-checking
+
         return couponUserService.create(user, coupon).toCouponUserResult()
     }
 }
--- src/test/java/kr/hhplus/be/server/integration/usecase/coupon/CouponIssueTest.kt
+++ src/test/java/kr/hhplus/be/server/integration/usecase/coupon/CouponIssueTest.kt
@@ -1,16 +1,21 @@
 package kr.hhplus.be.server.integration.usecase.coupon
 
+import jakarta.transaction.Transactional
 import kr.hhplus.be.server.application.coupon.CouponFacade
 import kr.hhplus.be.server.domain.coupon.CouponDiscountType
 import kr.hhplus.be.server.domain.coupon.CouponEntity
 import kr.hhplus.be.server.domain.user.UserEntity
-import kr.hhplus.be.server.domain.user.UserRepository
 import kr.hhplus.be.server.infra.coupon.CouponJpaRepository
+import kr.hhplus.be.server.infra.coupon.CouponUserJpaRepository
+import kr.hhplus.be.server.infra.user.UserJpaRepository
 import org.junit.jupiter.api.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.redis.core.RedisTemplate
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.atomic.AtomicInteger
+import kotlin.jvm.optionals.getOrNull
+import kotlin.test.assertEquals
 
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -19,37 +24,43 @@ class CouponIssueTest {
     @Autowired
     private lateinit var couponFacade: CouponFacade
 
-
     @Autowired
     private lateinit var couponJpaRepository: CouponJpaRepository
 
     @Autowired
-    private lateinit var userRepository: UserRepository
+    private lateinit var couponUserJpaRepository: CouponUserJpaRepository
+
+    @Autowired
+    private lateinit var userJpaRepository: UserJpaRepository
+
+    @Autowired
+    private lateinit var redisTemplate: RedisTemplate<String, String>
 
     private lateinit var coupon: CouponEntity
     private lateinit var user: UserEntity
 
     @BeforeEach
     fun init() {
-        val couponId = 0L
-        val userId = 0L
+        couponUserJpaRepository.deleteAllInBatch()
+        couponJpaRepository.deleteAllInBatch()
+        userJpaRepository.deleteAllInBatch()
         coupon = couponJpaRepository.saveAndFlush(
             CouponEntity(
-                id = couponId,
                 name = "COUPON1",
-                capacity = 1,
+                capacity = 100,
+                maxCapacity = 100,
                 discountValue = 10,
                 discountType = CouponDiscountType.AMOUNT
             )
         )
-        user = userRepository.saveAndFlush(UserEntity(id = userId, name = "testUser", 0))
-
+        user = userJpaRepository.saveAndFlush(UserEntity(name = "testUser", balance = 0))
+        redisTemplate.delete("coupon:${coupon.id}")
     }
+
 
     @Test
     fun `쿠폰 발행 비관락 테스트`() {
         // given
-
         val threadCount = 10 // 동시 실행할 스레드 수
         val successCount = AtomicInteger(0) // 성공 횟수
         val failCount = AtomicInteger(0)    // 실패 횟수
@@ -74,4 +85,60 @@ class CouponIssueTest {
         // 실패 횟수는 threadCount - 1이어야 함
         Assertions.assertEquals(threadCount - 1, failCount.get())
     }
+
+    @Test
+    @Transactional
+    @DisplayName("[성공] 쿠폰 발행 성공(단건)")
+    fun issueCoupon() {
+        couponFacade.issue(coupon.id, user.id)
+        val coupon: CouponEntity? = couponJpaRepository.findById(coupon.id).getOrNull()
+        Assertions.assertTrue(coupon?.capacity == 99)
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("[성공] 쿠폰 발행 성공(수량 만큼)")
+    fun issueAllCoupon() {
+        val coupon: CouponEntity = couponJpaRepository.findById(coupon.id).orElseThrow()
+
+        val savedUsers = userJpaRepository.saveAllAndFlush((1..coupon.maxCapacity).map {
+            UserEntity(name = "testUser:$it", balance = 0)
+        })
+
+        savedUsers.forEach { user ->
+            couponFacade.issue(coupon.id, user.id)
+        }
+        Assertions.assertTrue(coupon?.capacity == 0)
+    }
+
+
+    @Test
+    @DisplayName("[예외] 이미 지급된 쿠폰일 경우 예외 발생")
+    fun alreadyIssuedCoupon() {
+        couponFacade.issue(coupon.id, user.id)
+        val exception = assertThrows<IllegalStateException> {
+            couponFacade.issue(coupon.id, user.id)
+        }
+        assertEquals("해당 쿠폰을 이미 발급 받았습니다.", exception.message)
+    }
+
+    @Test
+    @DisplayName("[예외] 쿠폰 수량이 소진되었을 때 예외 발생")
+    fun soldOutCoupon() {
+        val savedUsers = userJpaRepository.saveAllAndFlush((1..100L).map {
+            UserEntity(name = "testUser:$it", balance = 0)
+        })
+
+        savedUsers.forEach { user ->
+            couponFacade.issue(coupon.id, user.id)
+        }
+
+        val lastUser = userJpaRepository.saveAndFlush(UserEntity(name = "testUser", balance = 99))
+        val exception = assertThrows<IllegalStateException> {
+            couponFacade.issue(coupon.id, lastUser.id)
+        }
+        assertEquals("쿠폰 수량이 소진 되었습니다.", exception.message)
+    }
+
+
 }
